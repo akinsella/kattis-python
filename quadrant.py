39cfc4648e18d47de63e5ce41c9a8da8da8e8208
Alexis Kinsella <alexis.kinsella@gmail.com>
2022-01-08 18:57:16 +0100

    Added 'quadrant' challenge

---
+++ quadrant.py
@@ -0,0 +1,13 @@
+import sys
+
+x = int(next(sys.stdin))
+y = int(next(sys.stdin))
+
+if x >= 0 and y >= 0:
+    print(1)
+elif x >= 0 and y < 0:
+    print(4)
+elif x < 0 and y < 0:
+    print(3)
+else:
+    print(2)
